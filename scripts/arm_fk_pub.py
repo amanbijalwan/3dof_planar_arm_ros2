d9dbe1bc02ffcae9da3d13d1ddc3dd4bd76a6cc1
amanbijalwan <amanbijalwan@gmail.com>
2026-07-24 17:43:03 +0000

    Updated motion to linear interpolation. In between joint motion.

--- scripts/arm_fk_pub.py
+++ scripts/arm_fk_pub.py
@@ -3,44 +3,52 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
-import math
-import time
+import numpy as np
 
-class ArmPublisher(Node):
+class ScaraPublisher(Node):
     def __init__(self):
-        super().__init__('arm_fk_publisher')
-        self.publisher = self.create_publisher(JointState, 'joint_states', 10)
-        self.timer = self.create_timer(0.1, self.timer_callback)
+        super().__init__('scara_fk_publisher')
+        self.pub = self.create_publisher(JointState, 'joint_states', 10)
+        self.timer = self.create_timer(0.05, self.update)
 
-        # Defining set of points (joint angles in radians)
+        #points: [joint1, joint2, joint3]
         self.points = [
-            [0.0, 0.0],
-            [0.5, 0.5],
-            [1.0, -0.5],
-            [0.0, 1.0]
+            [0.0, 0.0, 0.08],
+            [0.5, 0.5, -0.08],
+            [1.0, -0.5, 0.08],
+            [0.0, 1.0, -0.08]
         ]
-        self.index = 0
+        self.current = 0
+        self.next = 1
+        self.steps = 50   #ip steps
+        self.step_count = 0
 
-    def timer_callback(self):
+    def update(self):
+        # Linear interpolation test
+        start = np.array(self.points[self.current])
+        end   = np.array(self.points[self.next])
+        alpha = self.step_count / self.steps
+        pos   = (1 - alpha) * start + alpha * end
+
         msg = JointState()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.name = ['joint1', 'joint2'] 
-        msg.position = self.points[self.index]
+        msg.name = ['joint1', 'joint2', 'joint3']
+        msg.position = pos.tolist()
 
-        self.publisher.publish(msg)
-        self.get_logger().info(f'Publishing {msg.position}')
+        self.pub.publish(msg)
 
-        # Cycling through points...
-        self.index = (self.index + 1) % len(self.points)
-        time.sleep(1.0)  # pause bw points
+        self.step_count += 1
+        if self.step_count > self.steps:
+            self.step_count = 0
+            self.current = self.next
+            self.next = (self.next + 1) % len(self.points)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = ArmPublisher()
+    node = ScaraPublisher()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-
